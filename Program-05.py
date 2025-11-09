8459077cf6723be8f64c2c25935f90bbb4fce139
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-10 02:21:10 +0530

    Fifth Program

---
+++ Program-05.py
@@ -0,0 +1,11 @@
+# This is a program to compare two numbers entered by the user.
+# It prints True if n1 is greater than or equal to n2. If not, it prints False.
+# This is a formula. [N1 >= N2]
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+result = number1 >= number2
+
+print("The result is : ",result)
