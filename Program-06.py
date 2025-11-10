7fbd0245cc5a26fa5cde513c86085344862a8953
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-11 02:15:16 +0530

    Sixth Program

---
+++ Program-06.py
@@ -0,0 +1,10 @@
+# This is a program to print the sum of two numbers entered by the user.
+# This is a formula to calculate the sum of two numbers. [S = N1 + N2]
+
+number1 = float(input("Enter the first number : "))
+
+number2 = float(input("Enter the second number : "))
+
+sum = number1 + number2
+
+print("The sum of two numbers is : ",sum)
