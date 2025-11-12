c3dcd3524f98580ad41106e76577179942c2c8b4
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-13 01:26:51 +0530

    Eighth Program

---
+++ Program-08.py
@@ -0,0 +1,10 @@
+# This is a program to print the average of two numbers entered by the user.
+# This is a formula to calculate the average of two numbers. [A = (N1 + N2) / 2]
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+average = (number1 + number2) / 2
+
+print("The average of two numbers is : ",average)
