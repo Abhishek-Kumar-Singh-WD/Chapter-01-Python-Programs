43d74f21a7cf30b199037338a3761bbd30ec2923
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-17 21:46:32 +0530

    Tenth Program

---
+++ Program-10.py
@@ -0,0 +1,10 @@
+# This is a program to print the area of a circle based on the radius entered by the user.
+# This is a formula to calculate the area of a circle. [A = P * R * R]
+
+pai = 3.14159
+
+radius = int(input("Enter the radius of a circle : "))
+
+area = pai * radius * radius
+
+print("The area of a circle is : ",area)
