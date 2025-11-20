b2246812640d12f9752955177a796ae0ccfe083d
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-20 21:17:41 +0530

    Thirteenth Program

---
+++ Program-13.py
@@ -0,0 +1,10 @@
+# This is a program to print the perimeter of a rectangle using the height and width entered by the user.
+# This is a formula to calculate the perimeter of a rectangle. [P = (H + W) * 2]
+
+height = float(input("Enter the height of a rectangle : "))
+
+width = float(input("Enter the width of a rectangle : "))
+
+perimeter = (height + width) * 2
+
+print("The perimeter of a rectangle is : ",perimeter)
