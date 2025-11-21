c8022f1b02e8119e5ec4b44b4f9b3c0f156c3cf4
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-11-21 22:03:00 +0530

    Fourteenth Program

---
+++ Program-14.py
@@ -0,0 +1,8 @@
+# This is a program to print the cube of a number entered by the user.
+# This is a formula to calculate the cube of a number. [C = N * N * N]
+
+number = int(input("Enter a number : "))
+
+cube = number * number * number
+
+print("The cube of a number is : ",cube)
