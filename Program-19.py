a0a50c1a2e7db54f85c8fd0c837e6b132b20e42a
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-05 22:13:02 +0530

    Nineteenth Program

---
+++ Program-19.py
@@ -0,0 +1,10 @@
+# This is a program to print the difference of two numbers entered by the user.
+# This is a formula to calculate the difference of two numbers. [D = N1 - N2]
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+difference = number1 - number2
+
+print("The difference of two numbers is : ",difference)
