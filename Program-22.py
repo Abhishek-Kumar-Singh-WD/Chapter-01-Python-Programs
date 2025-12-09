f72e1276015d43f357d7d96592f20da8ca7c8337
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-10 01:53:08 +0530

    Twenty-second Program

---
+++ Program-22.py
@@ -0,0 +1,10 @@
+# This is a program to print the multiplication of two numbers entered by the user.
+# This is a formula to calculate the multiplication of two numbers. [M = N1 * N2]
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+multiplication = number1 * number2
+
+print("The multiplication of two numbers is : ",multiplication)
