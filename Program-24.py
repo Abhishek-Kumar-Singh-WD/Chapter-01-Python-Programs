c97ba01eca853d0515e55816b2009d5affb12c2c
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-12 22:33:31 +0530

    Twenty-fourth Program

---
+++ Program-24.py
@@ -0,0 +1,10 @@
+# This is a program to print the division of two numbers entered by the user.
+# This is a formula to calculate the division of two numbers. [D = N1 / N2]
+
+number1 = float(input("Enter the first number : "))
+
+number2 = float(input("Enter the second number : "))
+
+division = number1 / number2
+
+print("The division of two numbers is : ",division)
