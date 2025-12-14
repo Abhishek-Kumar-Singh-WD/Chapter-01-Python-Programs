c951b54c1b90911d34aa6ca5ae4f2d29e576107f
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-15 02:46:06 +0530

    Twenty-Sixth Program

---
+++ Program-26.py
@@ -0,0 +1,10 @@
+# This is a program to print the remainder of two numbers entered by the user.
+# This is a formula to calculate the remainder of two numbers. [R = N1 % N2]
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+remainder = number1 % number2
+
+print("The remainder of two numbers is : ",remainder)
