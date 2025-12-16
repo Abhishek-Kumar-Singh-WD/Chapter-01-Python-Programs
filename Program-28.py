d9f810efeb89997d4f373b7ff968fd18e7f86c0d
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-17 02:17:49 +0530

    Twenty-Eighth Program

---
+++ Program-28.py
@@ -0,0 +1,8 @@
+# This is a program to print the area of a square based on the side entered by the user.
+# This is a formula to calculate the area of a square. [A = S ** 2]
+
+side = float(input("Enter the side of a square : "))
+
+area = side ** 2
+
+print("The area of a square is : ",area)
