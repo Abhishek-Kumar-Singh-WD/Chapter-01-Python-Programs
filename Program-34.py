bb2da71561a88c9b72d653cad0164dc4a129f0ac
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-12-25 22:39:38 +0530

    Thirty-fourth Program

---
+++ Program-34.py
@@ -0,0 +1,8 @@
+# This is a program to print the square of a number entered by the user.
+# This is a formula to calculate the square of a number. [S = N ** 2]
+
+number = int(input("Enter a number : "))
+
+square = number ** 2
+
+print("The square of a number is : ",square)
