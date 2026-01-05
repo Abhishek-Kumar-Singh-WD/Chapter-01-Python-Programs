7ff657a1e49af2daa57898aa9c65affc97d66ef3
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2026-01-05 22:22:20 +0530

    Thirty-sixth Program

---
+++ Program-36.py
@@ -0,0 +1,8 @@
+# This is a program to print the cube of a number entered by the user.
+# This is a formula to calculate the cube of a number. [C = N ** 3]
+
+number = float(input("Enter a number : "))
+
+cube = number ** 3
+
+print("The cube of a number is : ",cube)
