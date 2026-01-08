215dde9d008b5f6321722741f347e03154847069
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2026-01-08 21:22:21 +0530

    Thirty-nineth Program

---
+++ Program-39.py
@@ -0,0 +1,7 @@
+# This is a program to check the type of data entered by the user.
+
+data = float(input("Enter the point number : "))
+
+data_type = type(data)
+
+print("The type of data is : ",data_type)
